98c1fddc041c26b1cde607c77c76b89e1df152c8
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2022-10-13 10:03:06 -0300

    delete

--- aula01/app.js
+++
@@ -1,7 +0,0 @@
-var ingredientes = ['mel','água', 'sal', 'mostarda']
-
-for(var i = 0; i < ingredientes.length; i++){
-    var letraInicial = 
-        ingredientes[i].charAt(0).toUpperCase();
-    console.log(ingredientes[i]);
-}
